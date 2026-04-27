5185bf78e3b403e5c7426392277950f73eaac66c
Andre Chumbimuni <tu-correo@ejemplo.com>
2026-04-26 19:05:45 -0500

    quitandole comentarios obvios

--- benchmark/benchmark_lfu.cpp
+++ benchmark/benchmark_lfu.cpp
@@ -11,7 +11,6 @@ int main() {
     // Iniciar cronómetro
     auto inicio = std::chrono::high_resolution_clock::now();
 
-    // Hacer 100,000 inserciones y lecturas
     for (int i = 0; i < operaciones; i++) {
         cache.put(i, i * 2);
         cache.get(i / 2); // Leemos elementos aleatorios
@@ -25,7 +24,7 @@ int main() {
     std::cout << "Tiempo total: " << duracion.count() << " milisegundos." << std::endl;
     
     if (duracion.count() < 100) {
-        std::cout << "¡Rendimiento excelente! Complejidad O(1) confirmada." << std::endl;
+        std::cout << "Complejidad O(1) confirmada." << std::endl;
     }
 
     return 0;
